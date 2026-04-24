bedba8f260e46f0d48794dbfa4325236194be620
Archana Kumar <2005archanakumar2005@gmail.com>
2026-04-24 23:40:11 +0530

    Update map.js

--- map.js
+++ map.js
@@ -4,12 +4,36 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap'
 }).addTo(map);
 
+let markers = [];
+let allData = [];
+
 fetch('data.json')
   .then(res => res.json())
   .then(data => {
-    data.forEach(loc => {
-      L.marker([loc.lat, loc.lng])
-        .addTo(map)
-        .bindPopup(`<b>${loc.name}</b><br>${loc.type}`);
-    });
+    allData = data;
+    displayMarkers(data);
   });
+
+function displayMarkers(data) {
+  // Remove existing markers
+  markers.forEach(marker => map.removeLayer(marker));
+  markers = [];
+
+  // Add new markers
+  data.forEach(loc => {
+    let marker = L.marker([loc.lat, loc.lng])
+      .addTo(map)
+      .bindPopup(`<b>${loc.name}</b><br>${loc.type}`);
+
+    markers.push(marker);
+  });
+}
+
+function filterMarkers(category) {
+  if (category === 'all') {
+    displayMarkers(allData);
+  } else {
+    let filtered = allData.filter(loc => loc.type === category);
+    displayMarkers(filtered);
+  }
+}
