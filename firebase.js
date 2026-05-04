f26820a86dfe80c7668f8ccd361686f056a6943b
Archana Kumar <2005archanakumar2005@gmail.com>
2026-05-04 07:42:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -15,15 +15,22 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 /* 🔴 ADD YOUR FIREBASE CONFIG HERE */
-const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "XXX",
-  appId: "XXX"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyASYxHX5-H7DiVbB_Q0UeqlBrGobyspPb8",
+    authDomain: "delhi-temp.firebaseapp.com",
+    projectId: "delhi-temp",
+    storageBucket: "delhi-temp.firebasestorage.app",
+    messagingSenderId: "1063718083614",
+    appId: "1:1063718083614:web:99332fba7a1ba6350d220f",
+    measurementId: "G-2YRP8GBDNP"
+  };
 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 /* INIT */
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
